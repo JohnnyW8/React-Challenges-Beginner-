89345a0933fb5430cbdedd215fee52c5491cc28d
Janusz Wnorowski <januszwno@gmail.com>
2019-06-15 23:14:41 +0200

    Start challenge.

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ScotchInfoBar from './ScotchInfoBar';
+import './styles.css';
+
+const user = {
+    name: 'Chris on Code',
+    location: 'Las Vegas',
+    foodType: 'Everything',
+    age: 28,
+    likes: 'Coding into the wee hours of the morning',
+    twitterUsername: 'chrisoncode',
+    avatar:
+      'https://scotch-res.cloudinary.com/image/upload/v1556479698/xRZsnhr0_400x400_cpyg2t.png'
+  };
+
+function App() {
+    return (
+      <div className="App">
+        <div className="user-deets">
+            {/* show data here */}
+        </div>
+        <ScotchInfoBar />
+      </div>
+    );
+  }
+
+  const rootElement = document.getElementById('root');
+  ReactDOM.render(<App />, rootElement);
