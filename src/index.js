33ddda0ec2ce455a318fc807203794f787c768aa
Janusz Wnorowski <januszwno@gmail.com>
2019-06-16 20:49:35 +0200

    Completed lesson 4.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import ScotchInfoBar from './ScotchInfoBar';
 import './styles.css';
 
 function App() {
+  const [word, setWord] = useState('');
+
+  if(word === 'alert') alert('Show alert');
+  
     return (
       <div className="App">
         <h2>What's the secret phrase?</h2>
   
-        <input type="text" placeholder="Super duper secret" />
+        <input type="text" placeholder="Super duper secret" onChange={e => setWord(e.target.value)} />
   
         <p>
           Hint: It's <strong>open sesame</strong>
